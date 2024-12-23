ee1f9c0a2883cf991ede56e42637f81ddaaced59
mauromltn <mauromtne@icloud.com>
2024-12-23 12:19:27 +0100

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Mauro Montané Meliá on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

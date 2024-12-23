35406a18a8406c2d7d9dcdb5c45bf4324cfa5e66
mauromltn <mauromtne@icloud.com>
2024-12-23 17:56:03 +0100

    Create the Welcome Page

---
+++ OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+//  Created by Mauro Montané Meliá on 23/12/24.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}

6f894e813d03574ed96ebc7d3295fdc3d33e4a5d
mauromltn <mauromtne@icloud.com>
2024-12-26 00:16:57 +0100

    Create a third page

---
+++ OnboardingFlow/SignUpPage.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpPage.swift
+//  OnboardingFlow
+//
+//  Created by Mauro Montané Meliá on 25/12/24.
+//
+
+import SwiftUI
+
+struct SignUpPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpPage()
+}
